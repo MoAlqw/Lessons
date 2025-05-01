7c82756f31c6c9a02cfca2fbd9d5207e7815df46
MoAlqw <nikitaguzenko2@gmail.com>
2025-05-01 16:26:14 +0500

    Made BoxDrawingView functionality

--- 07 - DragAndDraw/app/src/main/java/com/example/draganddraw/BoxDrawingView.kt
+++ 07 - DragAndDraw/app/src/main/java/com/example/draganddraw/BoxDrawingView.kt
@@ -1,11 +1,58 @@
 package com.example.draganddraw
 
 import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.PointF
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 class BoxDrawingView(context: Context, attrs: AttributeSet? = null): View(context, attrs) {
 
+    private var currentBox: Box? = null
+    private val boxen = mutableListOf<Box>()
+    private val boxPaint = Paint().apply {
+        color = (0x22ff0000).toInt()
+    }
+    private val backgroundPaint = Paint().apply {
+        color = (0xfff8efe0).toInt()
+    }
 
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        event ?: return false
+        val current = PointF(event.x, event.y)
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                currentBox = Box(current).also {
+                    boxen.add(it)
+                }
+            }
+            MotionEvent.ACTION_MOVE -> {
+                updateCurrentBox(current)
+            }
+            MotionEvent.ACTION_UP -> {
+                updateCurrentBox(current)
+                currentBox = null
+            }
+            MotionEvent.ACTION_CANCEL -> {
+                currentBox = null
+            }
+        }
+        return true
+    }
 
+    override fun onDraw(canvas: Canvas) {
+        canvas.drawPaint(backgroundPaint)
+        boxen.forEach { box ->
+            canvas.drawRect(box.left, box.top, box.right, box.bottom, boxPaint)
+        }
+    }
+
+    private fun updateCurrentBox(current: PointF) {
+        currentBox?.let {
+            it.end = current
+            invalidate()
+        }
+    }
 }
